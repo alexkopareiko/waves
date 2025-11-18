38424f6175b41bf6f3390018c00e9f645c7585d4
Олександр Копарейко <sanyakopar@gmail.com>
2025-11-18 17:22:35 +0200

    build for windows

--- Assets/_Project/Scripts/Managers/SaveManager.cs
+++ Assets/_Project/Scripts/Managers/SaveManager.cs
@@ -1,13 +1,7 @@
 using UnityEngine;
-using System.Collections.Generic;
-#if UNITY_WEBGL
-using Playgama;
-#endif
 
-public partial class SaveManager : MonoBehaviour
+public class SaveManager : MonoBehaviour
 {
-    // Leaderboards: change this to your configured ID in playgama-bridge-config.json
-    public const string LeaderboardId = "best_score";
     public static SaveManager Instance => s_Instance;
     private static SaveManager s_Instance;
 
@@ -27,13 +21,6 @@ public partial class SaveManager : MonoBehaviour
         SetupInstance();
     }
 
-    private void Awake()
-    {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        InitializeBridgeStorage();
-#endif
-    }
-
     private void SetupInstance()
     {
         gameObject.name = "SaveManager " + UnityEngine.Random.Range(0f, 1f);
@@ -51,22 +38,6 @@ public partial class SaveManager : MonoBehaviour
 
     public void Reset()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        var keys = new List<string>
-        {
-            k_SoundVolume,
-            k_MusicVolume,
-            k_Vibration,
-            k_PostProcessing,
-            k_Privacy,
-            k_TutorialWatched,
-            k_RateUsClicked,
-            k_LosesCount,
-            k_MaxScore,
-            k_CurrentLevel
-        };
-        Bridge.storage.Delete(keys, null, null);
-#endif
         PlayerPrefs.DeleteAll();
     }
 
@@ -157,103 +128,15 @@ public partial class SaveManager : MonoBehaviour
     }
 
     #endregion
-}
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-public partial class SaveManager
-{
-    private readonly Dictionary<string, string> _cache = new();
+    #region PlayerPrefs helpers
 
-    private static readonly List<string> s_AllKeys = new()
-    {
-        k_SoundVolume,
-        k_MusicVolume,
-        k_Vibration,
-        k_PostProcessing,
-        k_Privacy,
-        k_TutorialWatched,
-        k_RateUsClicked,
-        k_LosesCount,
-        k_MaxScore,
-        k_CurrentLevel
-    };
-
-    private void InitializeBridgeStorage()
-    {
-        // Load all keys once to keep getters synchronous
-        Bridge.storage.Get(s_AllKeys, (success, values) =>
-        {
-            if (success && values != null)
-            {
-                for (int i = 0; i < s_AllKeys.Count && i < values.Count; i++)
-                {
-                    var key = s_AllKeys[i];
-                    var val = values[i];
-                    if (val != null)
-                    {
-                        _cache[key] = val;
-                    }
-                }
-            }
-        }, null);
-    }
-
-    private float GetFloat(string key, float def)
-    {
-        if (_cache.TryGetValue(key, out var str) && !string.IsNullOrEmpty(str))
-        {
-            if (float.TryParse(str, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out var f))
-                return f;
-        }
-        return PlayerPrefs.GetFloat(key, def);
-    }
-
-    private void SetFloat(string key, float value)
-    {
-        var s = value.ToString(System.Globalization.CultureInfo.InvariantCulture);
-        _cache[key] = s;
-        PlayerPrefs.SetFloat(key, value);
-        Bridge.storage.Set(key, s, null, null);
-    }
-
-    private int GetInt(string key, int def)
-    {
-        if (_cache.TryGetValue(key, out var str) && !string.IsNullOrEmpty(str) && int.TryParse(str, out var i))
-            return i;
-        return PlayerPrefs.GetInt(key, def);
-    }
-
-    private void SetInt(string key, int value)
-    {
-        var s = value.ToString();
-        _cache[key] = s;
-        PlayerPrefs.SetInt(key, value);
-        Bridge.storage.Set(key, s, null, null);
-    }
-
-    private bool GetBool(string key, bool def)
-    {
-        if (_cache.TryGetValue(key, out var str) && !string.IsNullOrEmpty(str) && bool.TryParse(str, out var b))
-            return b;
-        return PlayerPrefs.GetInt(key, def ? 1 : 0) == 1;
-    }
-
-    private void SetBool(string key, bool value)
-    {
-        var s = value.ToString();
-        _cache[key] = s;
-        PlayerPrefs.SetInt(key, value ? 1 : 0);
-        Bridge.storage.Set(key, s, null, null);
-    }
-}
-#else
-public partial class SaveManager
-{
     private float GetFloat(string key, float def) => PlayerPrefs.GetFloat(key, def);
     private void SetFloat(string key, float value) => PlayerPrefs.SetFloat(key, value);
     private int GetInt(string key, int def) => PlayerPrefs.GetInt(key, def);
     private void SetInt(string key, int value) => PlayerPrefs.SetInt(key, value);
     private bool GetBool(string key, bool def) => PlayerPrefs.GetInt(key, def ? 1 : 0) == 1;
     private void SetBool(string key, bool value) => PlayerPrefs.SetInt(key, value ? 1 : 0);
+
+    #endregion
 }
-#endif
